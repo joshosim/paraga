fba8e20201e6a68d8cda7581931a8ca028e5faca
osimuka <osimukaokpan@gmail.com>
2024-04-08 08:52:48 +0100

    sign up refurbished

--- paragan/app/views.py
+++ paragan/app/views.py
@@ -3,8 +3,11 @@ from users.models import *
 from django.shortcuts import get_object_or_404
 
 # Create your views here.
+
+
 def home(request):
     return render(request, 'home.html')
+
 
 def dashboard(request):
     user_id = request.user.id
@@ -12,13 +15,16 @@ def dashboard(request):
     if User.is_authenticated:
         user_data = get_object_or_404(User, id=user_id)
 
-    return render(request, 'dashboard.html', {'user_data':user_data})
+    return render(request, 'dashboard.html', {'user_data': user_data})
+
 
 def investment(request):
     return render(request, 'investment.html')
 
+
 def paystack(request):
     return render(request, 'payStack.html')
 
+
 def toHome(request):
-    return render(request, 'toHome/html')
+    return render(request, 'toHome.html')
